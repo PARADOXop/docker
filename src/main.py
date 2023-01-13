72922470c574abc92702609113dbe86b31d846ef
PARADOXop <ravirajkukade11@gmail.com>
2023-01-13 12:48:20 +0530

    learning docker

--- src/main.py
+++ src/main.py
@@ -1,8 +1,5 @@
 from fastapi import FastAPI
 
-import redis
-
-r = redis.Redis(host="redis123", port=6379)
 app = FastAPI()
 
 # import debugpy
@@ -17,8 +14,3 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/hits")
-def read_root():
-    r.set("foo", "bar")
-    r.incr("hits")
-    return {"Number of hits:": r.get("hits"), "foo": r.get("foo")}
