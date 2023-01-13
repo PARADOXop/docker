db866d8b93018237b27506bf69524fa740f790da
PARADOXop <ravirajkukade11@gmail.com>
2023-01-13 12:23:48 +0530

    learning docker

--- src/main.py
+++ src/main.py
@@ -1,6 +1,24 @@
-from flask import Flask
-app = Flask(__name__)
+from fastapi import FastAPI
 
-@app.route('/')
-def hello_world():
-    return 'Hello, Docker!'
+import redis
+
+r = redis.Redis(host="redis123", port=6379)
+app = FastAPI()
+
+# import debugpy
+
+# debugpy.listen(("0.0.0.0", 5678))
+# print("Waiting for client to attach...")
+# debugpy.wait_for_client()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/hits")
+def read_root():
+    r.set("foo", "bar")
+    r.incr("hits")
+    return {"Number of hits:": r.get("hits"), "foo": r.get("foo")}
